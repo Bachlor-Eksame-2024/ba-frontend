772ff41bff90ed435a53b195939ff23bf65b5eb6
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-14 14:51:55 +0100

    Add SWR Data fecthing and Zustand State

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import useSWR from 'swr';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const { data, error } = useSWR('https://httpbin.org/get');
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <>
