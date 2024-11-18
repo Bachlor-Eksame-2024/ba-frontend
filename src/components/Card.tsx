102d2f335f5de57d24f5208535a5b0e17ebe26ab
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-18 16:33:53 +0100

    Made cards for home

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,73 @@
-function Card() {
+interface CardProps {
+  image?: string | null;
+  type?: 'regular' | 'workout' | 'workoutmini';
+  title?: string;
+  description?: string;
+  level?: 'Beginner' | 'Intermediate' | 'Advanced' | null;
+}
+
+function Card({ image, type = 'regular', title = 'Title', description = '', level }: CardProps) {
+  const RegularCard = () => {
+    return (
+      <div className='w-72 h-40 bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-4 relative cursor-pointer'>
+        <div className='flex flex-col z-[2]'>
+          <h4 className='text-3xl text-center'>{title}</h4>
+          <p>{description}</p>
+        </div>
+
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const WorkoutCard = () => {
+    return (
+      <div className='w-72 h-40 bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-4 relative cursor-pointer'>
+        <div className='absolute top-0 left-0 bg-purple-400 rounded-tl-lg rounded-br-lg'>
+          <span className='py-2 px-3 font-thin'>{level}</span>
+        </div>
+        <div className='flex flex-col justify-end h-full w-full items-start z-[2]'>
+          <h4 className='text-lg text-center'>{title}</h4>
+        </div>
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+  const WorkoutCardMini = () => {
+    return (
+      <div className='w-40 h-40 bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-2 relative cursor-pointer'>
+        <div className='absolute top-0 left-0 bg-purple-400 rounded-tl-lg rounded-br-lg z-[2]'>
+          <span className='py-2 px-3 font-thin'>{level}</span>
+        </div>
+        <div className='flex flex-col justify-end h-full w-full items-start z-[2]'>
+          <h4 className='text-base text-left'>{title}</h4>
+        </div>
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <div className='max-w-80 max-h-32 min-w-32 min-h-16 bg-red-300'>
-      <div>
-        <span className='text-red-500'>banner</span>
-      </div>
-      <div>
-        <h4>Title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, consequatur.</p>
-      </div>
-      <img src='' alt='' />
-    </div>
+    <>
+      {type === 'regular' && <RegularCard />}
+      {type === 'workout' && <WorkoutCard />}
+      {type === 'workoutmini' && <WorkoutCardMini />}
+    </>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,72 @@
 import Card from '../components/Card';
 
+const normalCards = [
+  {
+    type: 'regular' as const,
+    title: 'Book en Boks',
+    description: '',
+    level: null,
+    image:
+      'https://images.unsplash.com/photo-1597076537061-a6b58163aa45?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    type: 'regular' as const,
+    title: 'Workouts Denne Måned',
+    description: '',
+    level: null,
+    image: 'https://blog.pope.tech/wp-content/uploads/2023/08/Fruit-sales-by-unit.png',
+  },
+  {
+    type: 'regular' as const,
+    title: 'Udforsk Rummet',
+    description: '',
+    level: null,
+    image:
+      'https://images.unsplash.com/photo-1513351974182-1f36b4d965d8?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
+
+const miniCards = [
+  {
+    type: 'workoutmini' as const,
+    title: 'FULL BODY WORKOUT',
+    description: '',
+    level: 'Beginner' as const,
+    image:
+      'https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    type: 'workoutmini' as const,
+    title: 'STRONG AND STEADY',
+    description: '',
+    level: 'Advanced' as const,
+    image:
+      'https://images.unsplash.com/photo-1521804906057-1df8fdb718b7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    type: 'workoutmini' as const,
+    title: 'THREE DAY POWER',
+    description: '',
+    level: 'Intermediate' as const,
+    image:
+      'https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
+
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col gap-4'>
       <h1 className='text-red-500'>Home</h1>
-      <Card />
+      <div className='flex flex-col items-center gap-4'>
+        {normalCards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </div>
+      <div className='flex gap-4 overflow-auto'>
+        {miniCards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </div>
     </div>
   );
 }
