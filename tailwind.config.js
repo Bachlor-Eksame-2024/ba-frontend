796f715e3d9e8579e6575eae1c8eaa409e61249c
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-18 15:29:55 +0100

    Tailwind fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
