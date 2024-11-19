b5bf58f6bd20979551c1570fd6c9e3e4a231b524
Jonathan Weldon <90177221+jona6893@users.noreply.github.com>
2024-11-19 10:23:06 +0100

    17 create homepage (#32)
    * tailwind fix
    
    * Made cards for home
    
    * First step, somewhat working on mobile.
    
    * mobile
    
    * windows ci
    
    * back to linux

---
+++ src/components/Card.tsx
@@ -0,0 +1,74 @@
+interface CardProps {
+  image?: string | null;
+  type?: 'regular' | 'workout' | 'workoutmini';
+  title?: string;
+  description?: string;
+  level?: 'Beginner' | 'Intermediate' | 'Advanced' | null;
+}
+
+function Card({ image, type = 'regular', title = 'Title', description = '', level }: CardProps) {
+  const RegularCard = () => {
+    return (
+      <div className='min-w-72 min-h-40 aspect-video bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-4 relative cursor-pointer'>
+        <div className='flex flex-col z-[2]'>
+          <h4 className='text-3xl text-center'>{title}</h4>
+          <p>{description}</p>
+        </div>
+
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const WorkoutCard = () => {
+    return (
+      <div className='min-w-72 min-h-40 aspect-video bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-4 relative cursor-pointer'>
+        <div className='absolute top-0 left-0 bg-purple-400 rounded-tl-lg rounded-br-lg'>
+          <span className='py-2 px-3 font-thin'>{level}</span>
+        </div>
+        <div className='flex flex-col justify-end h-full w-full items-start z-[2]'>
+          <h4 className='text-lg text-center'>{title}</h4>
+        </div>
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+  const WorkoutCardMini = () => {
+    return (
+      <div className='min-w-40 min-h-40 aspect-square bg-default-200 rounded-lg flex flex-col justify-center items-center overflow-hidden p-2 relative cursor-pointer'>
+        <div className='absolute top-0 left-0 bg-purple-400 rounded-tl-lg rounded-br-lg z-[2]'>
+          <span className='py-2 px-3 font-thin'>{level}</span>
+        </div>
+        <div className='flex flex-col justify-end h-full w-full items-start z-[2]'>
+          <h4 className='text-base text-left'>{title}</h4>
+        </div>
+        {image && (
+          <div className='absolute w-full h-full'>
+            <div className='absolute top-0 w-full h-full bg-black/20 z-[1]'></div>
+            <img className='absolute top-0 object-cover w-full h-full' src={image} alt='' />
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {type === 'regular' && <RegularCard />}
+      {type === 'workout' && <WorkoutCard />}
+      {type === 'workoutmini' && <WorkoutCardMini />}
+    </>
+  );
+}
+
+export default Card;
---
+++ src/components/home/UserChartMobile.tsx
@@ -0,0 +1,69 @@
+import { Bar, BarChart, Rectangle, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
+
+const data = [
+  {
+    name: 'Uge 1',
+    pv: 4,
+  },
+  {
+    name: 'Uge 2',
+    pv: 3,
+  },
+  {
+    name: 'Uge 3',
+    pv: 4,
+  },
+  {
+    name: 'Uge 4',
+    pv: 2,
+  },
+];
+
+import { TooltipProps } from 'recharts';
+
+const CustomTooltip: React.FC<TooltipProps<number, string>> = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className='custom-tooltip bg-default-200 p-2 rounded-lg'>
+        <p className='label'>{`${label} : ${payload[0].value}`}</p>
+        <p className='desc'>Workouts</p>
+      </div>
+    );
+  }
+  return null;
+};
+
+function UserChartMobile() {
+  return (
+    <div className='relative bg-default-100 min-w-72 min-h-40 aspect-video rounded-lg flex justify-center items-center p-2 cursor-pointer'>
+      <div className='absolute top-0 left-0 flex flex-col justify-center h-full w-full items-center z-[2]'>
+        <h4 className='text-3xl text-center'>Workout</h4>
+      </div>
+      <ResponsiveContainer height='90%' width={'95%'}>
+        <BarChart
+          width={400}
+          height={200}
+          data={data}
+          margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: -8,
+          }}
+        >
+          <XAxis tick={{ fill: '#FFFFFF' }} dataKey='name' className='text-foreground-100' />
+          {/*      <YAxis tick={{ fill: '#FFFFFF' }} /> */}
+          <Tooltip cursor={false} content={<CustomTooltip />} />
+          <Bar
+            dataKey='pv'
+            radius={[5, 5, 0, 0]}
+            fill='#9353D3'
+            activeBar={<Rectangle fill='#7828C8' />}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default UserChartMobile;
