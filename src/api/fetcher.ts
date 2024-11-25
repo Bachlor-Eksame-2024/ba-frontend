ff8a4568c2c9410fd3cbf6a9dbfd7e1709df99e0
Jonathan Weldon <90177221+jona6893@users.noreply.github.com>
2024-11-25 11:19:47 +0100

    Booking confirmation card on user home page (#65)

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,3 +1,10 @@
 // fetcher.ts
+const api_key = import.meta.env.VITE_API_KEY;
+
 export const fetcher = (url: string) =>
-  fetch(url, { credentials: 'include' }).then((res) => res.json());
+  fetch(url, {
+    credentials: 'include',
+    headers: {
+      'X-API-Key': api_key,
+    },
+  }).then((res) => res.json());
