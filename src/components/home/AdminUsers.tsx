a2aea615bf8909e31843bb1277b110c4a8b72e4b
Jonathan Weldon <90177221+jona6893@users.noreply.github.com>
2024-11-27 14:48:25 +0100

    76 connect admin user to backend (#77)
    * admin users
    
    * Update user status

--- src/components/home/AdminUsers.tsx
+++ src/components/home/AdminUsers.tsx
@@ -1,68 +1,159 @@
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/table';
 import { Input, Pagination, Select, SelectItem } from '@nextui-org/react';
 import { useState } from 'react';
+import useAdminUsersStore from '../../stores/adminUsersStore';
+import useSWR from 'swr';
+import { AdminUser } from '../../types/user';
 
 function AdminUsers() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const variants = ['Aktiv', 'Afmeldt'];
   const [searchTerm, setSearchTerm] = useState('');
-  // Maks 10 brugere i fakeData, Hente henter brugere fra databasen ved brug af limit 10
-  const fakeData = [
-    { name: 'Tony Reichert', medlem: 'JA', center: 'Nørrebro', status: 'Aktiv' },
-    { name: 'Zoey Lang', medlem: 'JA', center: 'Østerbro', status: 'Afmeldt' },
-    { name: 'Jane Fisher', medlem: 'NEJ', center: 'Vensterbro', status: 'Inaktiv' },
-    { name: 'William Howard', medlem: 'JA', center: 'Amager', status: 'Aktiv' },
-    { name: 'Tony Reichert', medlem: 'JA', center: 'Nørrebro', status: 'Aktiv' },
-    { name: 'Zoey Lang', medlem: 'JA', center: 'Østerbro', status: 'Afmeldt' },
-    { name: 'Jane Fisher', medlem: 'NEJ', center: 'Vensterbro', status: 'Inaktiv' },
-    { name: 'William Howard', medlem: 'JA', center: 'Amager', status: 'Aktiv' },
-    { name: 'Tony Reichert', medlem: 'JA', center: 'Nørrebro', status: 'Aktiv' },
-    { name: 'Zoey Lang', medlem: 'JA', center: 'Østerbro', status: 'Afmeldt' },
-  ];
-
-  const variants = ['Aktiv', 'Inaktiv', 'Afmeldt'];
-
-  const filteredData = fakeData.filter((user) =>
-    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const [curPage, setCurPage] = useState(1);
+  const { adminUsers, setAdminUsers } = useAdminUsersStore();
+  const { data, error } = useSWR(
+    apiUrl + `/admin/get-users?fitness_center_id=${1}&page=${1}&page_size=10`,
+    {
+      onSuccess: (data) => {
+        setAdminUsers(data);
+      },
+    }
   );
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  // Before the return statement
+  if (!adminUsers) return null;
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
+  const handleChangePage = async (page: number) => {
+    const response = await fetch(
+      apiUrl + `/admin/get-users?fitness_center_id=${1}&page=${page}&page_size=10`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': import.meta.env.VITE_API_KEY as string,
+        },
+      }
+    );
+    const data = await response.json();
+    setAdminUsers(data);
   };
 
-  /* Logic for at ændre status på bruger skal implimenteres */
+  const handleSearchChange = async (
+    searchQuery: string,
+    page: number = 1,
+    isFromPagination: boolean = false
+  ) => {
+    // Only update searchTerm if not from pagination
+    if (!isFromPagination) {
+      setSearchTerm(searchQuery);
+    }
 
+    try {
+      const response = await fetch(
+        apiUrl +
+          `/admin/search-users?fitness_center_id=1&page=${page}&page_size=10&search_query=${searchQuery}`,
+        {
+          method: 'GET',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': import.meta.env.VITE_API_KEY as string,
+          },
+        }
+      );
+      const data = await response.json();
+
+      if (data.detail) {
+        handleChangePage(1);
+      } else {
+        setAdminUsers(data);
+      }
+    } catch (error) {
+      console.error('Search failed:', error);
+      handleChangePage(1);
+    }
+  };
+  // Input handler
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    handleSearchChange(value);
+  };
+
+  // Pagination handler
+  const handlePaginationChange = (page: number) => {
+    setCurPage(page);
+    if (searchTerm.trim() === '') {
+      handleChangePage(page);
+    } else {
+      handleSearchChange(searchTerm, page, true);
+    }
+  };
+
+  // update user Status
+  const handleUpdateUserStatus = async (userId: number, is_member: boolean) => {
+    try {
+      const response = await fetch(apiUrl + `/admin/update-membership`, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': import.meta.env.VITE_API_KEY as string,
+        },
+        body: JSON.stringify({
+          user_id: userId,
+          is_member: !is_member,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.message) {
+        console.log('User status updated successfully');
+        const newUsers = adminUsers;
+        newUsers.users = newUsers.users.map((user: AdminUser) => {
+          if (user.user_id === userId) {
+            user.is_member = !is_member;
+          }
+          return user;
+        });
+        setAdminUsers(newUsers);
+      }
+    } catch (error) {
+      console.error('Failed to update user status:', error);
+    }
+  };
+
+  // setup delete user in the future
+
+  console.log(adminUsers);
   return (
     <>
       <div className='sm:hidden grid gap-4'>
-        <Input onChange={handleSearchChange} type='text' label='Søg' />
-        {filteredData.map((user, index) => (
+        <Input onChange={handleInputChange} type='text' label='Søg' value={searchTerm} />
+        {adminUsers?.users?.map((user, index) => (
           <div
             key={index}
             className='grid grid-cols-2 gap-4 justify-between items-center bg-default-100 p-4 rounded-lg'
           >
             <div className='flex flex-col gap-2'>
-              <p className='font-bold'>{user.name}</p>
+              <p className='font-bold'>{user.first_name}</p>
 
               <p>
                 <span className='text-xs text-default-700'>Center</span>
                 <br />
-                {user.center}
+                currentCenter
               </p>
               <p>
                 <span className='text-xs text-default-700'>Medlemskab</span>
                 <br />
-                {user.medlem}
+                {user.is_member ? 'Ja' : 'Nej'}
               </p>
             </div>
             <Select
-              color={
-                user.status === 'Aktiv'
-                  ? 'success'
-                  : user.status === 'Inaktiv'
-                    ? 'warning'
-                    : 'danger'
-              }
+              color={user.is_member ? 'success' : 'danger'}
               label='Status'
-              defaultSelectedKeys={[user.status]}
+              defaultSelectedKeys={[user.is_member ? 'Aktiv' : 'Afmeldt']}
             >
               {variants.map((variant) => (
                 <SelectItem key={variant} value={variant}>
@@ -72,9 +163,18 @@ function AdminUsers() {
             </Select>
           </div>
         ))}
+        <Pagination
+          onChange={handlePaginationChange}
+          total={adminUsers?.total_pages || 1}
+          page={curPage}
+          loop
+          showControls
+          color='secondary'
+          initialPage={1}
+        />
       </div>
       <div className='max-sm:hidden grid gap-4 justify-items-center w-full'>
-        <Input onChange={handleSearchChange} type='text' label='Søg' />
+        <Input onChange={handleInputChange} type='text' label='Søg' value={searchTerm} />
         <Table aria-label='Example static collection table'>
           <TableHeader>
             <TableColumn>NAVN</TableColumn>
@@ -83,22 +183,19 @@ function AdminUsers() {
             <TableColumn>STATUS</TableColumn>
           </TableHeader>
           <TableBody>
-            {filteredData.map((user, index) => (
+            {adminUsers.users?.map((user, index) => (
               <TableRow key={index}>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.center}</TableCell>
-                <TableCell>{user.medlem}</TableCell>
+                <TableCell>
+                  {user.first_name} {user.last_name}
+                </TableCell>
+                <TableCell>set to current</TableCell>
+                <TableCell>{user.is_member ? 'Ja' : 'Nej'}</TableCell>
                 <TableCell>
                   <Select
-                    color={
-                      user.status === 'Aktiv'
-                        ? 'success'
-                        : user.status === 'Inaktiv'
-                          ? 'warning'
-                          : 'danger'
-                    }
-                    label='Status'
-                    defaultSelectedKeys={[user.status]}
+                    onChange={() => handleUpdateUserStatus(user.user_id, user.is_member)}
+                    className='w-32'
+                    color={user.is_member ? 'success' : 'danger'}
+                    defaultSelectedKeys={[user.is_member ? 'Aktiv' : 'Afmeldt']}
                   >
                     {variants.map((variant) => (
                       <SelectItem key={variant} value={variant}>
@@ -111,7 +208,15 @@ function AdminUsers() {
             ))}
           </TableBody>
         </Table>
-        <Pagination loop showControls color='secondary' total={5} initialPage={1} />
+        <Pagination
+          onChange={handlePaginationChange}
+          total={adminUsers?.total_pages || 1}
+          page={curPage}
+          loop
+          showControls
+          color='secondary'
+          initialPage={1}
+        />
       </div>
     </>
   );
