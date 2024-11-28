e6705d982625e41d549eb3a65567e6de02db31be
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-28 13:37:55 +0100

    Calendar for mobile

--- src/components/booking/Calendar.tsx
+++ src/components/booking/Calendar.tsx
@@ -1,11 +1,44 @@
-import { DatePicker } from '@nextui-org/react';
+import { Input } from '@nextui-org/react';
+import { Calendar as CalendarNext } from '@nextui-org/react';
+import { useRef, useState } from 'react';
 
 export default function Calendar() {
-  const variant = 'bordered';
+  const [isOpen, setOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState('');
+  const calendarRef = useRef<HTMLDivElement>(null);
+
+  const handleDate = (value: { day: number; month: number; year: number }) => {
+    const day = value.day;
+    const month = value.month;
+    const year = value.year;
+    const dateStr = `${day}/${month}/${year}`;
+    setSelectedDate(dateStr);
+    setOpen(false);
+  };
+
+  const handleBlur = (event: React.FocusEvent<Element>) => {
+    if (calendarRef.current && calendarRef.current.contains(event.relatedTarget)) {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
-    <div>
+    <div className='relative'>
       <h2 className='text-xl font-bold mb-4 mt-8'>Vælg dato</h2>
-      <DatePicker variant={variant} color='secondary' className='max-w-[100%]' />
+      <Input
+        value={selectedDate}
+        placeholder={selectedDate}
+        onFocus={() => setOpen(true)}
+        onBlur={handleBlur}
+        type='text'
+        isReadOnly
+      />
+      {isOpen && (
+        <div className='absolute w-fit left-1/2 transform -translate-x-1/2' ref={calendarRef}>
+          <CalendarNext onChange={handleDate} aria-label='Date (No Selection)' />
+        </div>
+      )}
     </div>
   );
 }
