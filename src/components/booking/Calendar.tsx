9185e227962cdcfc5d009f2ed0e1b600dc772608
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-28 13:55:20 +0100

    calendar icon

--- src/components/booking/Calendar.tsx
+++ src/components/booking/Calendar.tsx
@@ -47,6 +47,28 @@ export default function Calendar() {
         type='text'
         isReadOnly
       />
+      <svg
+        className='absolute pointer-events-none top-[3.4rem] right-5'
+        height='21'
+        viewBox='0 0 21 21'
+        width='21'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g
+          fill='none'
+          fillRule='evenodd'
+          stroke='currentColor'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          transform='translate(2 2)'
+        >
+          <path d='m2.5.5h12c1.1045695 0 2 .8954305 2 2v12c0 1.1045695-.8954305 2-2 2h-12c-1.1045695 0-2-.8954305-2-2v-12c0-1.1045695.8954305-2 2-2z' />
+          <path d='m.5 4.5h16' />
+          <path d='m8.5 7.5v6.056' />
+          <path d='m8.5 7.5v6' transform='matrix(0 1 -1 0 19 2)' />
+        </g>
+      </svg>
+
       {isOpen && (
         <div className='absolute w-fit left-1/2 transform -translate-x-1/2' ref={calendarRef}>
           <CalendarNext
