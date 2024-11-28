d187e1a523f2c0688b2157801130edf694f7b547
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-28 13:47:06 +0100

    Gets currect date

--- src/components/booking/Calendar.tsx
+++ src/components/booking/Calendar.tsx
@@ -1,18 +1,20 @@
 import { Input } from '@nextui-org/react';
 import { Calendar as CalendarNext } from '@nextui-org/react';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { today, getLocalTimeZone, CalendarDate } from '@internationalized/date';
 
 export default function Calendar() {
   const [isOpen, setOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState('');
+  const [calendarDate, setCalendarDate] = useState(today(getLocalTimeZone()));
   const calendarRef = useRef<HTMLDivElement>(null);
-
   const handleDate = (value: { day: number; month: number; year: number }) => {
     const day = value.day;
     const month = value.month;
     const year = value.year;
     const dateStr = `${day}/${month}/${year}`;
     setSelectedDate(dateStr);
+    setCalendarDate(new CalendarDate(value.year, value.month, value.day));
     setOpen(false);
   };
 
@@ -22,6 +24,16 @@ export default function Calendar() {
     }
     setOpen(false);
   };
+  const formatDate = (date: CalendarDate) => {
+    const day = date.day;
+    const month = date.month;
+    const year = date.year;
+    return `${day}/${month}/${year}`;
+  };
+
+  useEffect(() => {
+    setSelectedDate(formatDate(calendarDate));
+  }, [calendarDate]);
 
   return (
     <div className='relative'>
@@ -37,7 +49,11 @@ export default function Calendar() {
       />
       {isOpen && (
         <div className='absolute w-fit left-1/2 transform -translate-x-1/2' ref={calendarRef}>
-          <CalendarNext onChange={handleDate} aria-label='Date (No Selection)' />
+          <CalendarNext
+            value={calendarDate}
+            onChange={handleDate}
+            aria-label='Date (No Selection)'
+          />
         </div>
       )}
     </div>
