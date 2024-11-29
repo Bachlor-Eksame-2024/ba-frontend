5b9f8776f65275e854b21470eeec0b2bfff57c64
Jonathan Weldon <jona6893@stud.kea.dk>
2024-11-29 12:22:27 +0100

    removed import react

--- src/pages/BookingProcess.tsx
+++ src/pages/BookingProcess.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import TimeSlotSelector from '../components/booking/TimeSlotSelector';
 import Payment from '../components/booking/Payment';
 
