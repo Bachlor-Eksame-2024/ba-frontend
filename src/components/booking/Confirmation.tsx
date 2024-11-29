335f1edcd7b49e9c913a4ab302fab55dfb8878f3
Helbob <millehelbo@outlook.dk>
2024-11-29 12:43:25 +0100

    Colors added an d stuff

--- src/components/booking/Confirmation.tsx
+++ src/components/booking/Confirmation.tsx
@@ -1,11 +1,12 @@
 function Confirmation() {
   return (
     <div>
+      <p className='success confp'>Du har successfuldt booket boks 4</p>
       <svg version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 130.2 130.2'>
         <circle
           className='path circle confsvg'
           fill='none'
-          stroke='#73AF55'
+          stroke='#7828C8'
           strokeWidth='6'
           strokeMiterlimit='10'
           cx='65.1'
@@ -15,14 +16,14 @@ function Confirmation() {
         <polyline
           className='path check'
           fill='none'
-          stroke='#73AF55'
+          stroke='#7828C8'
           strokeWidth='6'
           strokeLinecap='round'
           strokeMiterlimit='10'
           points='100.2,40.2 51.5,88.8 29.8,67.5 '
         />
       </svg>
-      <p className='success confp'>Oh Yeah!</p>
+      <p className='success confp'>Din kode er: 1234</p>
     </div>
   );
 }
