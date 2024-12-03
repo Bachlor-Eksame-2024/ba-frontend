d6bba123086b46842f217a0f7ee15ddc772b9a9c
Jonathan Weldon <jona6893@stud.kea.dk>
2024-12-03 12:18:41 +0100

    Admin bokse

--- src/components/home/AdminBokse.tsx
+++ src/components/home/AdminBokse.tsx
@@ -11,36 +11,88 @@ import { Pagination } from '@nextui-org/pagination';
 import { Select, SelectItem } from '@nextui-org/select';
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/table';
 import { useState } from 'react';
+import useSWR from 'swr';
+import { Boks, BoksData } from '../../types/AdminBoks';
+import { Tab, Tabs } from '@nextui-org/tabs';
+import { Card, CardBody } from '@nextui-org/card';
+
+const apiURL = import.meta.env.VITE_API_URL;
 
 function AdminBokse() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const [selectedBoks, setSelectedBoks] = useState<number | null>(null);
-  // maks 10 bokse af gang vi henter bokse fra databasen ved hjælp af limit 10
-  const fakeData = [
-    { boks: 1, status: 'Ledig', tider: ['06:00 - 07:00', '07:00-08:00'] },
-    { boks: 2, status: 'Booket', tider: ['10:00 - 12:00', '12:00-13:00'] },
-    { boks: 3, status: 'Lukket', tider: ['14:00 - 16:00', '16:00-20:00'] },
-  ];
+  const [selectedBoks, setSelectedBoks] = useState<BoksData | null>(null);
+  const [updateArray, setUpdateArray] = useState([0, 10]);
+  // the id need to come from userInfo when we have the login system ready
+  const { data, error } = useSWR(apiURL + '/admin/get-boks?fitness_center_id=1');
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   const variants = ['Book', 'Frigiv', 'Luk'];
+
+  const changePage = (page: number) => {
+    if (page === 1) {
+      setUpdateArray([0, 10]);
+    }
+    if (page === 2) {
+      setUpdateArray([10, 20]);
+    }
+    if (page === 3) {
+      setUpdateArray([20, 30]);
+    }
+  };
+
+  const getBoksTimes = async (boks: number) => {
+    const response = await fetch(
+      apiURL + '/admin/get-boks-avaliability-by-id?fitness_center_id=1&boks_id=' + boks,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': import.meta.env.VITE_API_KEY as string,
+        },
+      }
+    );
+    const data = await response.json();
+    setSelectedBoks(data);
+  };
 
   const TiderModal = () => {
     return (
-      <Modal className='bg-default-800 text-white' isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal
+        size='5xl'
+        className='bg-default-800 text-white'
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className='flex flex-col gap-1'>Tider</ModalHeader>
+              <ModalHeader className='flex flex-col gap-1'>
+                Boks {selectedBoks?.box_id} - Tider
+              </ModalHeader>
               <ModalBody>
-                {fakeData
-                  .find((boks) => boks.boks === selectedBoks)
-                  ?.tider.map((tid, index) => <p key={index}>{tid}</p>)}
+                <div className='flex w-full flex-col'>
+                  <Tabs className='dark' aria-label='Options'>
+                    {selectedBoks?.dates &&
+                      Object.keys(selectedBoks.dates).map((date, index) => (
+                        <Tab key={index} title={date}>
+                          <Card className='dark'>
+                            <CardBody>
+                              <ul>
+                                {Object.keys(selectedBoks.dates[date]).map((time, index) => (
+                                  <li key={index}>{time} - Fri</li>
+                                ))}
+                              </ul>
+                            </CardBody>
+                          </Card>
+                        </Tab>
+                      ))}
+                  </Tabs>
+                </div>
               </ModalBody>
               <ModalFooter>
-                <Button color='danger' variant='light' onPress={onClose}>
-                  Close
-                </Button>
-                <Button color='primary' onPress={onClose}>
-                  Action
+                <Button color='secondary' onPress={onClose}>
+                  Luk
                 </Button>
               </ModalFooter>
             </>
@@ -61,13 +113,13 @@ function AdminBokse() {
             <TableColumn>HANDLING</TableColumn>
           </TableHeader>
           <TableBody>
-            {fakeData.map((boks, index) => (
+            {data.boks.slice(updateArray[0], updateArray[1]).map((boks: Boks, index: number) => (
               <TableRow key={index}>
-                <TableCell>{boks.boks}</TableCell>
-                <TableCell>{boks.status}</TableCell>
+                <TableCell>Nr. {boks.box_number}</TableCell>
+                <TableCell>{boks.box_availability}</TableCell>
                 <TableCell
                   onClick={() => {
-                    setSelectedBoks(boks.boks);
+                    getBoksTimes(boks.box_number);
                     onOpen();
                   }}
                   className='cursor-pointer'
@@ -77,14 +129,14 @@ function AdminBokse() {
                 <TableCell>
                   <Select
                     color={
-                      boks.status === 'Ledig'
+                      boks.box_availability === 'available'
                         ? 'success'
-                        : boks.status === 'Booket'
+                        : boks.box_availability === 'Booket'
                           ? 'warning'
                           : 'danger'
                     }
-                    label='Handling'
-                    defaultSelectedKeys={[boks.status]}
+                    label={boks.box_availability}
+                    defaultSelectedKeys={[boks.box_availability]}
                   >
                     {variants.map((variant) => (
                       <SelectItem key={variant} value={variant}>
@@ -98,7 +150,15 @@ function AdminBokse() {
           </TableBody>
         </Table>
         <TiderModal />
-        <Pagination loop showControls color='secondary' total={5} initialPage={1} />
+        <span className='text-sm text-gray-300'>Bokse total: {data.boks.length}</span>
+        <Pagination
+          onChange={(page) => changePage(page)}
+          loop
+          showControls
+          color='secondary'
+          total={3}
+          initialPage={1}
+        />
       </div>
     </section>
   );
---
+++ src/types/AdminBoks.ts
@@ -0,0 +1,22 @@
+export interface Boks {
+  box_number: number;
+  box_availability: string;
+  fitness_center_fk: number;
+  box_id: number;
+  created_at: string;
+}
+
+export interface BoksData {
+  box_id: number;
+  dates: bokDate;
+}
+export interface bokDate {
+  [date: string]: BoksTime;
+}
+export interface BoksTime {
+  [time: string]: BoksAvailabilty;
+}
+export interface BoksAvailabilty {
+  available: boolean;
+  booking: string;
+}
