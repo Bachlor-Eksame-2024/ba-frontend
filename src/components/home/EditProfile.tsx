b131d585bab36a0ed28a7347108394289f7ed21d
Jonathan Weldon <jona6893@stud.kea.dk>
2024-12-05 12:48:52 +0100

    changes so far

--- src/components/home/EditProfile.tsx
+++ src/components/home/EditProfile.tsx
@@ -1,5 +1,6 @@
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@nextui-org/modal';
 import { Button, Input } from '@nextui-org/react';
+import { useState } from 'react';
 
 interface EditProfileProps {
   isOpen: boolean;
@@ -7,6 +8,26 @@ interface EditProfileProps {
 }
 
 function EditProfile({ isOpen, onOpenChange }: EditProfileProps) {
+  const [passwordError, setPasswordError] = useState('');
+  const handleChangePassword = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const old_password = formData.get('old_password');
+    const new_password = formData.get('new_password');
+    console.log(old_password);
+    const confirm_password = formData.get('confirm_password');
+    if (new_password !== confirm_password) {
+      setPasswordError('Adgangskoderne er ikke ens');
+      return;
+    }
+  };
+
+  const handleUpdateProfile = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    console.log(formData);
+  };
+
   return (
     <Modal className='dark' size='5xl' isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent className=' bg-default-50 text-white'>
@@ -14,26 +35,34 @@ function EditProfile({ isOpen, onOpenChange }: EditProfileProps) {
           <>
             <ModalHeader className='flex flex-col gap-1'>Bruger Infomation</ModalHeader>
             <ModalBody className='grid sm:grid-cols-2'>
-              <div className='grid gap-4'>
+              <form onSubmit={handleUpdateProfile} className='grid gap-4'>
                 <h3 className='text-md'>Opdater Personinfomation</h3>
                 <Input type='text' label='Fornavn' />
                 <Input type='text' label='Efternavn' />
                 <Input type='email' label='Email' />
                 <Input type='tel' label='Telefon' />
                 <Input type='text' label='Center' />
-              </div>
-              <div className='flex flex-col gap-4 '>
+                <Button type='submit' className='bg-secondary text-white py-6'>
+                  Opdater Profile
+                </Button>
+              </form>
+              <form onSubmit={handleChangePassword} className='flex flex-col gap-4 '>
                 <h3 className='text-md'>Skift Din Adgangskode</h3>
-                <Input type='password' label='Nuværende Adgangskode' />
-                <Input type='password' label='Nyt Adgangskode' />
-                <Input type='password' label='Gentage Adgangskode' />
-                <Button className='bg-secondary text-white py-6'>Skift Adgangskode</Button>
-              </div>
+                <span className='text-danger text-sm'>{passwordError}</span>
+                <Input name='old_password' type='password' required label='Nuværende Adgangskode' />
+                <Input name='new_password' type='password' required label='Nyt Adgangskode' />
+                <Input
+                  name='confirm_password'
+                  type='password'
+                  required
+                  label='Gentage Adgangskode'
+                />
+                <Button type='submit' className='bg-secondary text-white py-6'>
+                  Skift Adgangskode
+                </Button>
+              </form>
             </ModalBody>
             <ModalFooter>
-              <Button color='danger' variant='light' onPress={onClose}>
-                Luk
-              </Button>
               <Button className='bg-secondary text-white' onPress={onClose}>
                 Gem
               </Button>
