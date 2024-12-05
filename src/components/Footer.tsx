1ee068883c09db8bbf613314ba393ce19636f944
Helbob <millehelbo@outlook.dk>
2024-12-05 12:46:49 +0100

    Added logo + responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,29 +5,29 @@ function Footer() {
   const { isAuthenticated } = useAuth();
 
   return (
-    <footer
-      className={`${isAuthenticated ? 'bg-default-100' : 'bg-default-50'} flex justify-between p-10`}
-    >
-      <div className='ml-8'>
-        <img src={logo} alt='Company Logo' className='h-8 mt-4' />
-      </div>
-      <div className='flex gap-20 mr-8'>
-        <div className='text-sm'>
-          <h3 className='text-base mb-2'>Firma</h3>
-          <p>Fitboks A/S</p>
-          <p>Adresse 32</p>
-          <p>1550 København</p>
-          <p>CVR: 12345678</p>
+    <footer className={`${isAuthenticated ? 'bg-default-100' : 'bg-default-50'} p-10`}>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-center md:text-left text-center'>
+        <div className='mb-4 md:mb-0'>
+          <img src={logo} alt='Company Logo' className='h-8' />
         </div>
-        <div className='text-sm'>
-          <h3 className='text-base mb-2'>Information</h3>
-          <p>Standardbetingelser</p>
-          <p>Cookie- og persondatapolitik</p>
-        </div>
-        <div className='text-sm'>
-          <h3 className='text-base mb-2'>Kontakt</h3>
-          <p>+45 12 34 56 78</p>
-          <p>ba-fitboks@outlook.com</p>
+        <div className='flex flex-col md:flex-row gap-10 md:gap-20 mt-4'>
+          <div className='text-sm'>
+            <h3 className='text-base mb-2'>Firma</h3>
+            <p>Fitboks A/S</p>
+            <p>Adresse 32</p>
+            <p>1550 København</p>
+            <p>CVR: 12345678</p>
+          </div>
+          <div className='text-sm'>
+            <h3 className='text-base mb-2'>Information</h3>
+            <p>Standardbetingelser</p>
+            <p>Cookie- og persondatapolitik</p>
+          </div>
+          <div className='text-sm mb-20'>
+            <h3 className='text-base mb-2'>Kontakt</h3>
+            <p>+45 12 34 56 78</p>
+            <p>ba-fitboks@outlook.com</p>
+          </div>
         </div>
       </div>
     </footer>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@nextui-org/react';
 import { Link } from 'wouter';
+import logo from '../assets/logo/logo.svg';
 
 export default function LandingPage() {
   return (
     <div className="relative h-screen w-full bg-[url('./assets/HealthFitness1.jpeg')] bg-cover bg-center">
       <div className='absolute inset-0 bg-gray-800 opacity-50 backdrop-blur-sm'></div>
-      <div className='relative z-10 p-4'>Logo</div>
+      <div className='relative z-10 p-4'>
+        <img src={logo} alt='Company Logo' className='h-8 md:hidden' />
+      </div>
       <div className='relative z-10 flex items-center justify-center sm:justify-start h-full px-4 sm:pl-28 w-full sm:w-6/12'>
         <div className='text-center sm:text-left'>
           <h1 className='text-3xl pb-4 sm:pb-8'>FÅ 50% DE FØRSTE 2 MDR. + FRI OPRETTELSE</h1>
