f75f4e1c177f8cd6becc5c1d26905ad26f6aa8bb
Helbob <millehelbo@outlook.dk>
2024-12-20 13:30:37 +0100

    rettet

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,10 +12,10 @@ export default function LandingPage() {
           <div className='md:hidden max-md:absolute top-0 z-10 p-4'>
             <img src={logo} alt='Company Logo' className='h-8 md:hidden' />
           </div>
-          <div className='relative z-10 flex items-center justify-center  sm:justify-start h-full px-4 sm:pl-28 w-full sm:w-6/12'>
+          <div className='relative z-10 flex items-center justify-center  sm:justify-start h-full px-4 sm:pl-0 w-full sm:w-3/4'>
             <div className='text-center sm:text-left flex flex-col gap-4'>
-              <h1 className='text-3xl pb-4 sm:pb-8'>FÅ 50% DE FØRSTE 2 MDR. + FRI OPRETTELSE</h1>
-              <p className='text-lg sm:text-xl pb-4 sm:pb-8'>
+              <h1 className='text-2xl pb-4 sm:pb-2'>FÅ 50% DE FØRSTE 2 MDR. + FRI OPRETTELSE</h1>
+              <p className='text-xl sm:text-md pb-4 sm:pb-8'>
                 Prøv 2 Bokse gratis, herefter 299,- md.
               </p>
               <div className='flex flex-col sm:flex-row gap-4'>
@@ -29,7 +29,7 @@ export default function LandingPage() {
                     type='submit'
                     color='default'
                     variant='bordered'
-                    className='w-full'
+                    className='w-full border-white bg-white bg-opacity-5'
                     size='lg'
                   >
                     Opret bruger
