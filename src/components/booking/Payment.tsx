86641e5c5b46ae7f9b3890a7e4885f16d67853ca
Jonathan Weldon <jona6893@stud.kea.dk>
2024-12-20 15:55:44 +0100

    Payment

--- src/components/booking/Payment.tsx
+++ src/components/booking/Payment.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@nextui-org/button';
-import { Card, CardBody } from '@nextui-org/card';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
 import { Elements, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { memo, useEffect, useState } from 'react';
@@ -19,8 +19,8 @@ const appearance = {
   theme: 'flat' as const,
   variables: {
     colorPrimary: '#0570de',
-    colorBackground: '#27272a',
-    colorText: '#ffffff',
+    colorBackground: 'white',
+    colorText: 'black',
     colorDanger: '#df1b41',
     fontFamily: 'Ideal Sans, system-ui, sans-serif',
     spacingUnit: '2px',
@@ -28,9 +28,9 @@ const appearance = {
   },
   rules: {
     '.Label': { color: 'white' },
-    '.Input': { color: 'white' },
+    '.Input': { color: 'black' },
     '.Input:focus': { borderColor: '#0570de' },
-    '.Tab': { color: '#ffffff' },
+    '.Tab': { color: 'black' },
     '.Tab:hover': { color: '#0570de' },
     '.Tab--selected': { borderColor: '#0570de' },
     '.Tab--selected:focus': { borderColor: '#0570de' },
@@ -112,7 +112,7 @@ const CheckoutForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='w-full bg-default-50 p-4 rounded flex flex-col gap-4'>
+    <form onSubmit={handleSubmit} className='w-full bg-default-100 p-4 rounded flex flex-col gap-4'>
       <PaymentElement />
       {errorMessage && <div className='text-danger'>{errorMessage}</div>}
       <Button type='submit' className='bg-secondary w-fit' isLoading={isLoading} disabled={!stripe}>
@@ -162,6 +162,7 @@ const Payment = memo(() => {
     };
 
     createPayment();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [collectedBooking]);
 
   if (!collectedBooking) {
@@ -175,20 +176,111 @@ const Payment = memo(() => {
 
   return (
     <div className='max-auto max-w-2xl mx-auto flex flex-col gap-4 items-center justify-center p-4 pt-20'>
-      <Card className='w-full'>
-        <CardBody>
-          <p>Boks: {collectedBooking.booking_box_id_fk}</p>
-          <p>Dato: {collectedBooking.booking_date}</p>
-          <p>Fra kl: {collectedBooking.booking_start_hour}</p>
-          <p>Til kl: {collectedBooking.booking_end_hour}</p>
-          <p>Pris: {collectedBooking.booking_duration_hours * 50},-</p>
+      <Card className='w-full bg-default-100 shadow-none mx-auto'>
+        <CardHeader>
+          <h3 className='text-md font-semibold text-center'>Bookingoversigt</h3>
+        </CardHeader>
+        <CardBody className='space-y-4'>
+          <div className='flex items-center space-x-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-box'
+            >
+              <path d='M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z' />
+              <path d='m3.3 7 8.7 5 8.7-5' />
+              <path d='M12 22V12' />
+            </svg>
+            <span className='font-semibold'>Box:</span>
+            <span>{collectedBooking.booking_box_id_fk}</span>
+          </div>
+          <div className='flex items-center space-x-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-calendar'
+            >
+              <path d='M8 2v4' />
+              <path d='M16 2v4' />
+              <rect width='18' height='18' x='3' y='4' rx='2' />
+              <path d='M3 10h18' />
+            </svg>
+            <span className='font-semibold'>Dato:</span>
+            <span>{new Date(collectedBooking.booking_date).toLocaleDateString()}</span>
+          </div>
+          <div className='flex items-center space-x-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='size-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+              />
+            </svg>
+            <span className='font-semibold'>Tid:</span>
+            <span>
+              {collectedBooking.booking_start_hour} - {collectedBooking.booking_end_hour}
+            </span>
+          </div>
+          <div className='flex items-center space-x-2'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth='1.5'
+              stroke='currentColor'
+              className='size-6'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+              />
+            </svg>
+
+            <span className='font-semibold'>Antal timer:</span>
+            <span>{collectedBooking.booking_duration_hours} </span>
+          </div>
+          <div className='mt-6 pt-4 border-t border-gray-200'>
+            <div className='flex items-center justify-between'>
+              <span className='text-base font-bold'>Total Pris:</span>
+              <div className='flex items-center space-x-2'>
+                {/* <CreditCardIcon className='text-red-500' /> */}
+                <span className='text-md font-semibold text-success'>
+                  {collectedBooking.booking_duration_hours * 50},- DKK
+                </span>
+              </div>
+            </div>
+          </div>
         </CardBody>
       </Card>
-      {clientSecret && (
-        <Elements stripe={stripePromise} options={options}>
-          <CheckoutForm />
-        </Elements>
-      )}
+      <Card className='w-full bg-default-100'>
+        {clientSecret && (
+          <Elements stripe={stripePromise} options={options}>
+            <CheckoutForm />
+          </Elements>
+        )}
+      </Card>
     </div>
   );
 });
